bd27a10d8b80bcd40b598a5398f3bc2625c128f4
Carlos Interone <carlos@mostlyclicks.com>
2018-04-16 21:36:28 -0600

    design nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,47 @@
 module ApplicationHelper
+
+	def logo
+		image_tag("Logo_GROUPE_2016.png")
+	end
+
+	def logo_picker(page)
+		if page_title == "Home"
+			link_to (image_tag "Logo_GROUPE_2016.png", class: "navbar-brand",alt: "Group Athlete", width: "200"), refinery.root_path
+		elsif page.title == "Rossignol" || page.root.title == "Rossignol"
+		 	 link_to (image_tag "Rossignol_Line_RED.png", class: "navbar-brand",alt: "Group Athlete / Rossignol", width: "200"), "/rossignol"
+		elsif page.title == "DLLK" || page.root.title == "DLLK"
+			 link_to (image_tag "Logo_Dynastar_15_horizontal.png", class: "navbar-brand",alt: "Group Athlete / DLLK", width: "150"), "/dllk"
+		end
+	end
+
+	def menu_picker(page_title, parent_id, page_root_id)
+		if page_title == "Home"
+			main_menu = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
+	    main_menu.menu_role = 'navigation'
+	    main_menu.menu_tag = "div"
+	    main_menu.css =  "collapse navbar-collapse"
+	    main_menu.dom_id = "navbarSupportedContent"
+	    main_menu.list_tag = "ul"
+	    main_menu.list_tag_css = "nav navbar-nav ml-auto"
+	    main_menu.list_item_tag = "li class=\"nav-item\""
+	    main_menu.selected_css = "active"
+	    main_menu.max_depth = 0
+	    main_menu.to_html
+		elsif page_title == page_title || page_root_id == parent_id
+			sub_menu = Refinery::Menu.new(refinery_menu_pages.select{ |item| item.parent_id == page_root_id })
+			sub_menu = Refinery::Pages::MenuPresenter.new(sub_menu, self)
+			sub_menu.menu_role = 'navigation'
+		  sub_menu.menu_tag = "div"
+		  sub_menu.css =  "collapse navbar-collapse"
+		  sub_menu.dom_id = "navbarSupportedContent"
+		  sub_menu.list_tag = "ul"
+		  sub_menu.list_tag_css = "nav navbar-nav ml-auto"
+		  sub_menu.list_item_tag = "li class=\"nav-item\""
+		  sub_menu.selected_css = "active"
+		  sub_menu.max_depth = 0
+		  sub_menu.to_html
+		end
+	end
+
+
 end
