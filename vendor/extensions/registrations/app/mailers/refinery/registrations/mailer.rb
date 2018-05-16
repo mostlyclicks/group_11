496ee1fe779f4f3e963d3152cfe05887f18ac3bb
mostlyclicks <carlos@mostlyclicks.com>
2018-05-16 17:02:58 -0600

    fix mailer and conf message

--- vendor/extensions/registrations/app/mailers/refinery/registrations/mailer.rb
+++ vendor/extensions/registrations/app/mailers/refinery/registrations/mailer.rb
@@ -4,7 +4,7 @@ module Refinery
 
       def confirmation(registration, request)
         @registration = registration
-        mail :subject  => Refinery::Registrations::Setting.confirmation_subject,
+        mail :subject  => Refinery::Registrations::Setting.confirmation_subject[locale.to_s],
              :to       => registration.email_address,
              :from     => "\"#{Refinery::Core.site_name}\" <no-reply@#{request.domain}>",
              :reply_to => Refinery::Registrations::Setting.notification_recipients.split(',').first
