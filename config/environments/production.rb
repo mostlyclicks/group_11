f8ae7d448fcd68eeb8f1b94b48d3491ef021fe80
mostlyclicks <carlos@mostlyclicks.com>
2018-08-13 18:45:11 -0600

    compile false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # config.serve_static_assets = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # config.assets.digest = true
 
