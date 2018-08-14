13a36d69c0046c764b014d0059ff36ac4368ee5a
mostlyclicks <carlos@mostlyclicks.com>
2018-08-13 18:55:53 -0600

    compile issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.assets.precompile = true
+ 
 
   # config.assets.digest = true
 
