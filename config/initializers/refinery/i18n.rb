a0076b7f818924cdbc7dea72a9d8812d0ad2a499
mostlyclicks <carlos@mostlyclicks.com>
2018-09-21 09:29:19 -0600

    config i18n

--- config/initializers/refinery/i18n.rb
+++ config/initializers/refinery/i18n.rb
@@ -7,7 +7,7 @@ Refinery::I18n.configure do |config|
 
   config.default_frontend_locale = :en
 
-  config.frontend_locales = [:en, :fr]
+  config.frontend_locales = [:en, :"fr-CA", :"en-CA"]
 
-  config.locales = {:en=>"English", :fr=>"Français", :nl=>"Nederlands", :pt=>"Português", :"pt-BR"=>"Português brasileiro", :da=>"Dansk", :nb=>"Norsk Bokmål", :sl=>"Slovenian", :es=>"Español", :ca=>"Català", :it=>"Italiano", :de=>"Deutsch", :lv=>"Latviski", :ru=>"Русский", :sv=>"Svenska", :pl=>"Polski", :"zh-CN"=>"简体中文", :"zh-TW"=>"繁體中文", :el=>"Ελληνικά", :rs=>"Srpski", :cs=>"Česky", :sk=>"Slovenský", :ja=>"日本語", :bg=>"Български", :hu=>"Hungarian", :uk=>"Українська"}
+  config.locales = {:en=>"English", :"en-CA"=>"English", :"fr-CA"=>"Français", :fr=>"Français", :nl=>"Nederlands", :pt=>"Português", :"pt-BR"=>"Português brasileiro", :da=>"Dansk", :nb=>"Norsk Bokmål", :sl=>"Slovenian", :es=>"Español", :ca=>"Català", :it=>"Italiano", :de=>"Deutsch", :lv=>"Latviski", :ru=>"Русский", :sv=>"Svenska", :pl=>"Polski", :"zh-CN"=>"简体中文", :"zh-TW"=>"繁體中文", :el=>"Ελληνικά", :rs=>"Srpski", :cs=>"Česky", :sk=>"Slovenský", :ja=>"日本語", :bg=>"Български", :hu=>"Hungarian", :uk=>"Українська"}
 end
